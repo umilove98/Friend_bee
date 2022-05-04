638975809b4d9f93c98d69d101777487c9108a24
juwon <80947350+1229juwon@users.noreply.github.com>
2022-05-04 22:17:24 +0900

    adapter

--- friendbee/app/src/main/java/com/example/friendsbee/ChatFragment.java
+++ friendbee/app/src/main/java/com/example/friendsbee/ChatFragment.java
@@ -3,10 +3,15 @@ package com.example.friendsbee;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +28,10 @@ public class ChatFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private RecyclerView recyclerView;
+    private ArrayList<WordItem> list = new ArrayList<>();
+    private ChatAdapter adapter;
+
 
     public ChatFragment() {
         // Required empty public constructor
@@ -59,6 +68,18 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.chatfragment_recyclerview);
+
+        list = WordItem.createContactsList(5);
+        recyclerView.setHasFixedSize(true);
+        adapter = new ChatAdapter(getActivity(), list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
+
+        Log.e("Frag", "MainFragment");
+
+
+        return view;
     }
 }
