e47cc238568e191a663b28872842c4c4fc15d968
godjieunbop <wldmswkd121@naver.com>
2022-05-12 03:04:14 +0900

    상세 카테고리 생성

---
+++ friendbee/app/src/main/java/com/example/friendsbee/FragmentHomeDetailMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.friendsbee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentHomeDetailMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home_detail_menu);
+    }
+}
