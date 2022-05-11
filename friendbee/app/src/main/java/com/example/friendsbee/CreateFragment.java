f72cbdb0948a97cd0e9cfd33bce407022efe3a4c
umilove98 <umilove98@kakao.com>
2022-05-11 22:59:21 +0900

    요청서 작성 화면 대충 구성

--- friendbee/app/src/main/java/com/example/friendsbee/CreateFragment.java
+++ friendbee/app/src/main/java/com/example/friendsbee/CreateFragment.java
@@ -2,11 +2,15 @@ package com.example.friendsbee;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+
+import com.example.friendsbee.databinding.ActivityMainBinding;
 
 /**
  * A simple {@link Fragment} subclass.
--- friendbee/app/src/main/java/com/example/friendsbee/MainActivity.java
+++ friendbee/app/src/main/java/com/example/friendsbee/MainActivity.java
@@ -6,12 +6,31 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+<<<<<<< Updated upstream
+=======
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+>>>>>>> Stashed changes
 
 import com.example.friendsbee.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+<<<<<<< Updated upstream
+=======
+    BottomNavigationView bottomNavigationView;
+    Menu menu;
+    ImageView menuIcon;
+    ImageView searchIcon;
+    TextView title;
+    private Spinner spinner;
+>>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +39,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
 
+<<<<<<< Updated upstream
+=======
+        spinner = findViewById(R.id.categorySpinner);
+
+        menuIcon = findViewById(R.id.menu_icon);
+        searchIcon = findViewById(R.id.search_icon);
+        title = findViewById(R.id.toolbar_title);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        menu = bottomNavigationView.getMenu();
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "메뉴 클릭됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "검색 버튼 클릭됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+>>>>>>> Stashed changes
         binding.bottomNavigationView.setOnItemSelectedListener(item ->{
 
             switch(item.getItemId()){
